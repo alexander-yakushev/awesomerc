303db669453b98a34e13a659a381ea8c61d1afab
Alexander Yakushev <alex@bytopia.org>
2014-07-30 23:31:32 +0300

    Update weather API

--- private.lua
+++ private.lua
@@ -3,9 +3,9 @@ local private = {}
 private.user = { name = "unlogic",
                  city = "Gjovik",
                  country = "Norway",
-                 time_offset = 1,
-                 loc =   { lat = 60.8, lon = 10.7 } }
+                 time_offset = 2,
+                 loc = { lat = 60.79, lon = 10.69 } }
 
-private.weather = { api_key = "sdccyyj9qax6atxuevd238tu" }
+private.weather = { api_key = "6f562795dda8824f99be11ea1eb0e77e" }
 
 return private
--- topjets/weather.lua
+++ topjets/weather.lua
@@ -5,69 +5,65 @@ local utility = require('utility')
 local iconic = require('iconic')
 local lustrous = require('lustrous')
 local format = string.format
+local json = require('json')
 
 local weather = {
-   cmd = "curl -s 'http://api.worldweatheronline.com/free/v1/weather.ashx?q=%s&format=csv&num_of_days=%s&key=%s' 2> /dev/null",
-   city = private.user.city .. "," .. private.user.country,
+   cmd = "curl -s 'https://api.forecast.io/forecast/%s/%s,%s?units=si&exclude=minutely,hourly,alerts,flags'",
+   lat = private.user.loc.lat, lon = private.user.loc.lon,
    api_key = private.weather.api_key,
-   days = 3
+   days = 2,
+   update_freq = 300,
 }
+
+local last_updated = nil
 
 local cond_mapping = {
-   ["Clear"] = { sym = "☀", sym_night = "☽", icon = 'weather-clear', icon_night = 'weather-clear-night' },
-   ["Sunny"] = { sym = "☀", sym_night = "☽", icon = 'weather-clear', icon_night = 'weather-clear-night' },
-   ["Partly Cloudy"] = { sym = "☁", icon = 'weather-few-clouds', icon_night = 'weather-few-clouds-night' },
-   ["Overcast"] = { sym = "☁", icon = 'weather-overcast' },
-   ["Cloudy"] = { sym = "☁", icon = 'weather-overcast' },
-   ["[Ff]og"] = { sym = "☁", icon = 'weather-fog.png' },
-   ["Mist"] = { sym = "☁", icon = 'weather-fog.png' },
-   ["outbreaks"] = { sym = "☂", icon = 'weather-storm' },
-   ["Patchy rain"] = { sym = "☂", icon = 'weather-showers-scattered' },
-   ["Light rain"] = { sym = "☂", icon = 'weather-showers-scattered' },
-   ["light rain"] = { sym = "☂", icon = 'weather-showers-scattered' },
-   ["Moderate rain"] = { sym = "☂", icon = 'weather-showers' },
-   ["Heavy rain"] = { sym = "☂", icon = 'weather-showers' },
-   ["rain shower"] = { sym = "☂", icon = 'weather-showers' },
-   ["drizzle"] = { sym = "☂", icon = 'weather-showers-scattered' },
-   ["with thunder"] = { sym = "☂", icon = 'weather-storm.png' },
-   ["Patchy sleet"] = { sym = "☃", icon = 'weather-snow.png' },
-   ["snow"] = { sym = "☃", icon = 'weather-snow.png' },
-   ["[Ss]leet"] = { sym = "☃", icon = 'weather-snow.png' },
-   ["[Ii]ce"] = { sym = "☃", icon = 'weather-snow.png' }
+   ["clear-day"]           = { sym = "☀", icon = 'weather-clear' },
+   ["clear-night"]         = { sym = "☽", icon = 'weather-clear-night' },
+   ["partly-cloudy-day"]   = { sym = "☁", icon = 'weather-few-clouds' },
+   ["partly-cloudy-night"] = { sym = "☁", icon = 'weather-few-clouds-night' },
+   ["cloudy"]              = { sym = "☁", icon = 'weather-overcast' },
+   ["wind"]                = { sym = "☁", icon = 'weather-overcast' },
+   ["fog"]                 = { sym = "☁", icon = 'weather-fog.png' },
+   ["rain"]                = { sym = "☂", icon = 'weather-showers' },
+   ["sleet"]               = { sym = "☃", icon = 'weather-snow.png' },
+   ["snow"]                = { sym = "☃", icon = 'weather-snow.png' },
 }
 
-local function command ()
-   return format (weather.cmd, weather.city, weather.days, weather.api_key)
+local function condition(icon_name)
+   return cond_mapping[icon_name] or cond_mapping["clear-day"]
 end
 
-local function widget_line (t)
-   return format(" %s%s°C",
-                 not t.icon and t.cond or "",
-                 t.temp)
+local function round(temp)
+   return math.floor(temp + 0.5)
+end
+
+local function command ()
+   return format (weather.cmd, weather.api_key, weather.lat, weather.lon )
 end
 
 local function forecast_line (t, today)
    if t == nil then return "ERROR" end
    local day, temp
    if today then
-      day, temp = "Today", t.temp .. "°C  "
+      day, temp = "Today", format("%d(%d)°C", round(t.temperature),
+                                  round(t.apparentTemperature))
    else
-      day, temp = t.date, t.temp_min .. "/" .. t.temp_max .. "°C"
+      day, temp = os.date("%d.%m", t.time), format("%d/%d°C", round(t.temperatureMin),
+                                                   round(t.temperatureMax))
    end
-   return format("%s\t%s\t%s%s",
-                 day, temp,
-                 (t.icon and (t.icon .. " ") or ""),
-                 (t.cond or ""))
+   return format("%s\t%s\t%s %s",
+                 day, temp, condition(t.icon).sym, (t.summary or ""))
 end
 
 function weather.update_tooltip (w)
-   weather.tooltip.title = forecast_line(w.weather.today, true)
-   weather.tooltip.icon = w.weather.today.large_icon
+   weather.tooltip.title = forecast_line(w.weather.currently, true)
+   weather.tooltip.icon = condition(w.weather.currently.icon).icon
 
    local text = ""
-   for i, t in ipairs(w.weather.forecast) do
-      text = text .. forecast_line(t)
-      if i < #w.weather.forecast then
+   for i = 2, weather.days + 1 do
+      text = text .. forecast_line(w.weather.daily.data[i])
+      if i < weather.days + 1 then
          text = text .. "\n"
       end
    end
@@ -81,57 +77,14 @@ end
 
 function weather.callback (file)
    local w = weather._widget
-   local i = 0
-   local m = "[^,]+"
-   w.weather = {}
-   for l in file:lines() do
-      if l:sub(1, 1) ~= "#" then
-         i = i + 1
-         if i == 1 then
-            local temp, desc =
-               l:match(format("%s,(%s),%s,%s,(%s),.*", m, m, m, m, m))
-            w.weather.today = { cond = desc or "", temp = temp or "" }
-            weather.process_entry(w.weather.today, true)
-            weather.w_text:set_markup(widget_line(w.weather.today))
-            weather.w_icon:set_image(w.weather.today.small_icon)
-            w.weather.forecast = {}
-         elseif i >= 3 then
-            local month, day, tempx, temp, desc =
-               l:match(format("%s+-(%s+)-(%s+),(%s),%s,(%s),%s,%s,%s,%s,%s,%s,%s,(%s),.*",
-                              "%d", "%d", "%d", m, m, m, m, m, m, m, m, m, m, m))
-               local t = { date = day .. "." .. month,
-                           cond = desc,
-                           temp_min = temp,
-                           temp_max = tempx }
-               weather.process_entry(t)
-               table.insert(w.weather.forecast, t)
-         end
-      end
-   end
-   file:close()
-   if w.weather.today then
+   w.weather = json.decode(file:read("*all"))
+   weather.w_text:set_markup(format("%d°C", round(w.weather.currently.temperature)))
+   weather.w_icon:set_image(cond_mapping[w.weather.currently.icon].icon or cond_mapping.clear.icon)
+   if w.weather.currently then
       weather.update_tooltip(weather._widget)
    end
-end
-
-function weather.process_entry (t, today)
-   for k, v in pairs(cond_mapping) do
-      if t.cond:match(k) then
-         t.icon = v.sym
-         t.large_icon = v.icon
-         t.small_icon = v.icon_small
-         if today and lustrous.get_time() == "night" then
-            if v.icon_night then
-               t.large_icon = v.icon_night
-               t.small_icon = v.icon_night_small
-            end
-            if v.sym_night then
-               t.icon = v.sym_night
-            end
-         end
-         break
-      end
-   end
+   file:close()
+   last_updated = os.time()
 end
 
 function weather.refresh()
@@ -142,12 +95,6 @@ function weather.new()
    for _, t in pairs(cond_mapping) do
       local icon_name = t.icon
       t.icon = iconic.lookup_status_icon(icon_name, { preferred_size = "128x128" })
-      t.icon_small = iconic.lookup_status_icon(icon_name, { preferred_size = "128x128" })
-      if t.icon_night then
-         local icon_name_night = t.icon_night
-         t.icon_night = iconic.lookup_status_icon(icon_name_night, { preferred_size = "128x128" })
-         t.icon_night_small = iconic.lookup_status_icon(icon_name_night, { preferred_size = "24x24" })
-      end
    end
 
    weather.w_icon = wibox.widget.imagebox()
@@ -166,7 +113,13 @@ function weather.new()
       timeout = 0,
       icon_size = 48 }
 
-   scheduler.register_recurring("topjets_weather", 60, weather.refresh)
+   scheduler.register_recurring("topjets_weather", 10,
+                                function()
+                                   if not last_updated or
+                                      (os.time() - last_updated > weather.update_freq) then
+                                      weather.refresh()
+                                   end
+                                end)
 
    utility.add_hover_tooltip(weather._widget,
                              function(w)
