c9e290895cb63d447873a36b4e5025f6039f7acd
Alexander Yakushev <alex@bytopia.org>
2015-05-31 14:17:08 +0200

    Minor changes

--- x220.lua
+++ x220.lua
@@ -292,7 +292,4 @@ client.connect_signal("manage",
                          end
 end)
 
-client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
-client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
-
 scheduler.start()
