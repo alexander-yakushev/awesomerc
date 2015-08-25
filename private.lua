236ed5fc9b78080aa8e615fe09f367d4ba683a3b
Alexander Yakushev <yakushev.alex@gmail.com>
2015-08-25 23:25:40 +0300

    Update private.lua

--- private.lua
+++ private.lua
@@ -3,6 +3,7 @@ local private = {}
 private.user = { name = "username",
                  loc = { lat = 51, lon = 5, gmt = 1 }}
 
+-- For weather API key register at https://forecast.io/
 private.weather = { api_key = "api key here" }
 
 return private
