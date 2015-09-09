c0f2b48d9fcbb6947e5b66dcca27ac8e4ec2d9a5
Alexander Yakushev <alex@bytopia.org>
2015-09-09 09:01:04 +0300

    Rework private.lua

--- private.lua
+++ private.lua
@@ -1,9 +1,25 @@
+local utility = require('utility')
+local util = require('awful.util')
+
 local private = {}
 
-private.user = { name = "username",
-                 loc = { lat = 51, lon = 5, gmt = 1 }}
+local locations =
+   { gjovik = { city = "Gjovik", country = "Norway",
+                lat = 60.79, lon = 10.69,
+                gmt = 1 },
+     ivfran = { city = "Ivano-Frankivsk",
+                country = "Ukraine",
+                lat = 48.92, lon = 24.71,
+                gmt = 2 },
+     kiev = { city = "Kiev", country = "Ukraine",
+              lat = 50.45, lon = 30.52,
+              gmt = 2 }}
 
--- For weather API key register at https://forecast.io/
-private.weather = { api_key = "api key here" }
+private.user = { name = "unlogic",
+                 loc = locations.ivfran }
+
+-- forecast.io API key is read from ./.forecast_io_api_key file
+private.weather = { api_key = utility.slurp(util.getdir("config") ..
+                                               "/.forecast_io_api_key", "*line") }
 
 return private
