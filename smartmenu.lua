484dd765cbb6ca6d05daadf0d947d401557259b2
Alexander Yakushev <alex@bytopia.org>
2015-09-25 11:14:46 +0300

    Netpower is not really necessary anymore

--- smartmenu.lua
+++ smartmenu.lua
@@ -1,13 +1,11 @@
 local menu = require('awful.menu')
 local util = require('awful.util')
+local utility = require('utility')
 
 local smartmenu = {}
 local scripts_dir = util.getdir("config") .. "/scripts/"
 
 local fm_script = scripts_dir .. "flashmanager"
-local np_script = scripts_dir .. "netpower"
-
-local fm_script_fmt = "sudo %s/scripts/flashmanager %s"
 
 local function flashmanager()
    if not util.file_readable(fm_script) then
@@ -26,19 +24,11 @@ local function flashmanager()
    end
 end
 
-local function netpower(action)
-   return string.format("sudo %s %s", np_script, action)
-end
-
 function smartmenu.show()
    local mainmenu = { items = {
                          { '&awesome', { { "restart", awesome.restart },
                                          { "quit", awesome.quit } } },
                          { '&flashmanager', flashmanager() },
-                         { '&network', { { "&Both", netpower("on on") },
-                                         { "&Ethernet", netpower("on off") },
-                                         { "&Wireless", netpower("off on") },
-                                         { "&Neither", netpower("off off") } } },
                          { '&music', function() utility.spawn_in_terminal("ncmpc") end },
                          { '&display', vista.xrandr.menu() } },
                       theme = { width = 150 } }
